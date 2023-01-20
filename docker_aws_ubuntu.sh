f795aa430c628d290650be11979ca0d29742bf7f
rehmanazhar922 <71656350+rehmanazhar922@users.noreply.github.com>
2023-01-20 19:05:32 +0500

    Update docker_aws_ubuntu.sh

--- docker_aws_ubuntu.sh
+++ docker_aws_ubuntu.sh
@@ -1,3 +1,3 @@
-#bash < <(https://raw.githubusercontent.com/rehmanazhar922/install/main/docker_aws_ubuntu.sh)
+#bash < <(curl https://raw.githubusercontent.com/rehmanazhar922/install/main/docker_aws_ubuntu.sh)
 sudo apt update
 sudo apt install docker* -y
