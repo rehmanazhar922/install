fd9d4bd52d2f9c0cef12e961196b0342bae0d7ef
rehmanazhar922 <71656350+rehmanazhar922@users.noreply.github.com>
2023-01-20 19:08:24 +0500

    Update python3_aws_ubuntu.sh

--- python3_aws_ubuntu.sh
+++ python3_aws_ubuntu.sh
@@ -1,3 +1,3 @@
-#bash < <(https://raw.githubusercontent.com/rehmanazhar922/install/main/python3_aws_ubuntu.sh)
+#bash < <(curl https://raw.githubusercontent.com/rehmanazhar922/install/main/python3_aws_ubuntu.sh)
 sudo apt update
 sudo apt install python3 -y
