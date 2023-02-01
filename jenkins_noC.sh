6dbbb01117ec8bf67b61ffcf41479d6da6a4fb68
rehmanazhar922 <71656350+rehmanazhar922@users.noreply.github.com>
2023-02-01 14:39:15 +0500

    Create jenkins_noC.sh

---
+++ jenkins_noC.sh
@@ -0,0 +1,24 @@
+#Jenkins has no installation candidate : error while trying to install jenkins on ubuntu system
+
+#Command 1
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+#command2 :
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+#command3:
+
+sudo apt-get update
+
+sudo apt-get install jenkins
+
+sudo systemctl status jenkins
+
+#Access the URL http://hostname:8080
+
+#use the below command to get the password
+
+#Password
+more /var/lib/jenkins/secrets/initialAdminPassword
